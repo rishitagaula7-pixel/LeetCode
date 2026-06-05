4babb4f61d5f00c598d4193edb9049fcd1e6087d
Rishita Gaula <rishitagaula7@gmail.com>
2026-06-05 10:15:40 +0530

    LC-powx-n [Runtime: N/A; Memory: N/A]

---
+++ recursion/powx-n.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        double ans=1.0;
+        long long nn=n;
+        if(nn<0) nn=-1*nn;//if n will be negetive i will make it positive 
+        while(nn){
+            if(nn%2==1){
+                ans=ans*x;
+                nn=nn-1;
+            }
+            else{
+                x=x*x;
+                nn=nn/2;
+
+            }
+        }
+    
+        if(n<0){
+            ans=(double)1/(double)(ans);
+        }
+            return ans;
+        
+        
+    }
+};
