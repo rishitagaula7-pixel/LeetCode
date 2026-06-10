30761f538a1acf076f9bc8a1f43a6f790e746bcb
Rishita Gaula <rishitagaula7@gmail.com>
2026-06-10 10:02:17 +0530

    LC-add-two-numbers [Runtime: 0 ms; Memory: 77.3 MB]

--- linked-list/add-two-numbers.cpp
+++ linked-list/add-two-numbers.cpp
@@ -1,28 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-        int carry = 0;
-
-        while (l1 != nullptr || l2 != nullptr || carry) {
-            int sum = carry;
-
-            if (l1) {
-                sum += l1->val;
-                l1 = l1->next;
+        ListNode* dummy= new ListNode();
+        ListNode* temp=dummy;
+        int carry=0;
+        while(l1!=NULL|| l2!=NULL|| carry){
+            int sum=0;
+            if(l1!= NULL){
+                sum+= l1->val;
+                l1= l1->next;
             }
-
-            if (l2) {
-                sum += l2->val;
-                l2 = l2->next;
+            if(l2!=NULL){
+                sum+=l2->val;
+                l2=l2->next;
             }
+            sum+=carry;
+            carry= sum/10;
+            ListNode* node=new ListNode(sum%10);
+            temp->next=node;
+            temp=temp->next;
 
-            carry = sum / 10;
-            curr->next = new ListNode(sum % 10);
-            curr = curr->next;
         }
-
         return dummy->next;
+        
     }
 };
