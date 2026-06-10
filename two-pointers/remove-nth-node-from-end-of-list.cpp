40b59125cdb53dda9099e5eb03da7502fe0820d0
Rishita Gaula <rishitagaula7@gmail.com>
2026-06-10 09:07:39 +0530

    LC-remove-nth-node-from-end-of-list [Runtime: N/A; Memory: N/A]

--- two-pointers/remove-nth-node-from-end-of-list.cpp
+++ two-pointers/remove-nth-node-from-end-of-list.cpp
@@ -1,26 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-
-        ListNode* fast = dummy;
-        ListNode* slow = dummy;
-
-        // Move fast ahead by n+1 steps
-        for (int i = 0; i <= n; i++) {
-            fast = fast->next;
+        ListNode* fast= head;
+        ListNode* slow= head;
+        for(int i=0;i<n;i++){
+            fast=fast->next;
         }
+         if(fast==NULL){
+                return head->next;
+            }
+            while(fast->next !=NULL){
+                slow=slow->next;
+                fast=fast->next;
 
-        // Move both pointers
-        while (fast != nullptr) {
-            fast = fast->next;
-            slow = slow->next;
-        }
+            }
+           
+            ListNode* delNode=slow->next;
+            slow->next=slow->next->next;
+           delete delNode;
 
-        // Delete nth node from end
-        slow->next = slow->next->next;
-
-        return dummy->next;
+            return head;
+        
+        
     }
 };
