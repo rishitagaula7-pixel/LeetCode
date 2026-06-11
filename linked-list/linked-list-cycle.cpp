06834e8c34b6001fb122941f6ed3f3607d64ac14
Rishita Gaula <rishitagaula7@gmail.com>
2026-06-11 15:27:37 +0530

    LC-linked-list-cycle [Runtime: N/A; Memory: N/A]

--- linked-list/linked-list-cycle.cpp
+++ linked-list/linked-list-cycle.cpp
@@ -1,25 +1,18 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode *slow= head;
-        ListNode *fast= head;
-        while(fast!=NULL&& fast->next!=NULL){
-            slow=slow->next;
-            fast = fast->next->next;
-            if(slow==fast){
-                return true;
-            }
+        ListNode *slow = head;
+        ListNode *fast = head;
 
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;   // Loop found
+            }
         }
-        return false;
-        
+
+        return false;   // No loop
     }
 };
