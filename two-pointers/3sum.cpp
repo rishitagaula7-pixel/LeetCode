2c952dbaff2b8d93d22ed57eb748fc54251f89ad
Rishita Gaula <rishitagaula7@gmail.com>
2026-06-13 16:28:24 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- two-pointers/3sum.cpp
+++ two-pointers/3sum.cpp
@@ -1,48 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;
-        int n = nums.size();
-        
-        // Step 1: Sort the array
-        sort(nums.begin(), nums.end());
-        
-        // Step 2: Fix the first element
-        for (int i = 0; i < n - 2; i++) {
-            
-            // Skip duplicate first elements
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-            
-            int left = i + 1;
-            int right = n - 1;
-            
-            // Step 3: Two pointer approach
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                
-                if (sum == 0) {
-                    result.push_back({nums[i], nums[left], nums[right]});
-                    
-                    // Skip duplicate second elements
-                    while (left < right && nums[left] == nums[left + 1])
-                        left++;
-                    
-                    // Skip duplicate third elements
-                    while (left < right && nums[right] == nums[right - 1])
-                        right--;
-                    
-                    left++;
-                    right--;
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(i>0&&nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k = nums.size() - 1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0) {
+                    j++;
                 }
-                else if (sum < 0) {
-                    left++;
+                else if(sum>0){
+                    k--;
                 }
-                else {
-                    right--;
+                else{
+                vector<int>temp={nums[i],nums[j],nums[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
                 }
+                while(j<k&&nums[j]==nums[j-1])j++;
+                while(j<k&&nums[k]==nums[k+1])k--;
             }
+
         }
-        return result;
+        return ans;
     }
 };
