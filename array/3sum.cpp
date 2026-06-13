511901eeb043dc444570990ecdc6d7afff21cbc3
Rishita Gaula <rishitagaula7@gmail.com>
2026-06-13 16:36:50 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- array/3sum.cpp
+++ array/3sum.cpp
@@ -22,11 +22,12 @@ for (int i = 0; i < n - 2; i++){
                 ans.push_back(temp);
                 j++;
                 k--;
- } while (j < k && nums[j] == nums[j - 1])
+ while (j < k && nums[j] == nums[j - 1])
         j++;
 
     while (j < k && nums[k] == nums[k + 1])
         k--;
+            }
             }
 
         }
