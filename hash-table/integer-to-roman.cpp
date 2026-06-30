fea53d8b9ea83f600c36da8bec43e1e1ad7573ce
Rishita Gaula <rishitagaula7@gmail.com>
2026-06-30 19:56:27 +0530

    LC-integer-to-roman [Runtime: N/A; Memory: N/A]

--- hash-table/integer-to-roman.cpp
+++ hash-table/integer-to-roman.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
-    string intToRoman(int num) {
-        vector<int> values = 
-            {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-
-        vector<string> symbols = 
-            {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-
-        string result = "";
-
-        for (int i = 0; i < values.size(); i++) {
-            while (num >= values[i]) {
-                result += symbols[i];
-                num -= values[i];
+    int romanToInt(string s) {
+        // Map to store Roman numeral values
+        // Using an array or switch is faster than unordered_map in C++
+        auto getValue = [](char c) {
+            switch(c) {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return 0;
+            }
+        };
+        
+        int total = 0;
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++) {
+            int currentVal = getValue(s[i]);
+            
+            // If current value is less than the next value, subtract it
+            if (i + 1 < n && currentVal < getValue(s[i + 1])) {
+                total -= currentVal;
+            } else {
+                total += currentVal;
             }
         }
-        return result;
+        
+        return total;
     }
 };
