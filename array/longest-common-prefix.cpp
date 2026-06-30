26e8f92040405af5c441daca45d215a0680bf575
Rishita Gaula <rishitagaula7@gmail.com>
2026-06-30 19:46:34 +0530

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

--- array/longest-common-prefix.cpp
+++ array/longest-common-prefix.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) return "";
-
-        for (int i = 0; i < strs[0].size(); i++) {
-            char c = strs[0][i];
-
-            for (int j = 1; j < strs.size(); j++) {
-                if (i >= strs[j].size() || strs[j][i] != c)
-                    return strs[0].substr(0, i);
+        if(strs.empty()) return "";
+        sort(strs.begin(),strs.end());
+        string first=strs[0];
+        string last = strs[strs.size()-1];
+        string ans="";
+        for(int i=0;i<strs.size();i++){
+            if(first[i]==last[i]){
+                ans+=first[i];
+            }
+            else{
+                break;
             }
         }
-        return strs[0];
+        return ans;
+        
     }
 };
